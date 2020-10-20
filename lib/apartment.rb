71b5ecb896e1e8d50a637e856f72a7a51a459f6e
Yesi Meza <yesi.meza10@gmail.com>
2020-10-20 09:42:46 -0600

    create and pass tests for apartment

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -3,11 +3,17 @@ class Apartment
 attr_reader :number,
             :monthly_rent,
             :bathrooms,
-            :bedrooms
+            :bedrooms,
+            :renter
   def initialize(data)
     @number       = data[:number]
     @monthly_rent = data[:monthly_rent]
     @bathrooms    = data[:bathrooms]
     @bedrooms     = data[:bedrooms]
+    @renter       = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter 
   end
 end
